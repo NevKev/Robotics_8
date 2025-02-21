4497a2f7aa7c3b349d1788a223c70692b92a9450
NevKev <nevint619@stemcollegiate.ca>
2025-02-21 10:44:02 -0700

    Update and rename Day1_100 to Day1_100.py

---
+++ Day1_100.py
@@ -0,0 +1,22 @@
+# Day 1:
+print ("I am signing up for Replit's 100 days of Python challenge! I will make sure to spend some time every day coding along, for a minimum of 10 minutes a day. I'll be using Replit, an amazing online IDE so I can do this from my phone wherever I happend to be. No excuses for not coding from the middle of a field!")
+print ("tanish so dumb")
+# Day 2: 
+myName = input ("What is your name? ")
+myLunch = input ("What are you having for lunch? ")
+print (myName, "is going to be chowing down on", myLunch, "very soon!")
+# Day 3:
+myFood = input("Name a food > ")
+myPlant = input("Name a type of plant > ")
+myCooking = input("Name a method of cooking > ")
+myBurned = input("What word describes burned food? > ")
+myItem = input("Name a DIY item > ")
+print("So you want , myFood, with , myPlant, on a bed of , myItem, ?")
+# Day 4:
+print ("Welcome to your customizable adventure sim! Please answer these questions to start!")
+myName = input("What is your name? ")
+myEnemy = input("What is your worst enemy’s name? ")
+mySuper = input("What is your superpower? ")
+myLocation = input ("Where do you live? ")
+myFood = input("What is your favorite food?")
+print("Hello" , myName , "! Your ability to" , mySuper , "will make sure you never have to look at" , myEnemy , "again. Go eat" , myFood , "as you walk down the streets of" , myLocation , "and use" , mySuper , "for \033[32mgood \033[0mand not \033[31mevil!")
