59eef9e5193a23d496986bbc6482bde98606cd81
NevKev <nevint619@stemcollegiate.ca>
2025-03-06 09:19:03 -0700

    Create Day5_100.py

---
+++ Day5_100.py
@@ -0,0 +1,18 @@
+myName = input("What's your name?: ")
+if myName == "Player":
+ print("Welcome Dude!")
+else:
+ print("Who on earth are you?!")
+ print("You are not welcome here! Go away!")
+ print("Actually, since im so bored im going to let you play")
+ print("Awsner the following questions")
+catsOrDogs = input("Are you a cat person? Or a dog person?: ")
+if catsOrDogs == "cat":
+  print("Meow")
+else:
+  print("Woof")
+  favoritecolour = print("Do you have a favorite colour?")
+  if favoritecolour == "yes":
+    print("thats cool")
+  else:
+    print("oh, well your lame")
