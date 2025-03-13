0db6256e9d32ef68dcac9416192badc03bd34c63
NevKev <nevint619@stemcollegiate.ca>
2025-03-13 08:48:36 -0600

    Create Day7_100.py

---
+++ Day7_100.py
@@ -0,0 +1,13 @@
+# This code if for Replit 100 Days of coding Day 7, this is what I have learned
+tvShow = input("What is your favourite tv show? ")
+if tvShow == "Wild Kratts":
+  print("Classic during elemertary lunch!")
+  faveCharacter = input("Who is your favourite character? ")
+  if faveCharacter == "Chris":
+    print("Right answer")
+  else:
+    print("Nah, Chris is the greatest")
+elif tvShow == "Spongebob":  # Moved elif to be after else
+  print("Thats what im talking about")
+else:)
+  print("Yeah, that's cool and all but spongebob is the greatest…"
