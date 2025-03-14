94303811d86e141e13258810ad314f6196d2a399
NevKev <nevint619@stemcollegiate.ca>
2025-03-14 10:13:58 -0600

    Create Day8_100.py

---
+++ Day8_100.py
@@ -0,0 +1,37 @@
+#Code for Day 8 of python coding on Replit
+print("Hello. Welcome to your daily affirmation generator.")
+name = input("What is your name? ")
+
+
+
+if name =="Nevin" or name == "nevin":
+  print("Hi, Nevin")
+else:
+  print("WHO ARE YOU")
+  myMon = input("What month is it?")
+if myMon == "January":
+  print("OOOOOO begining of the year huh", name)
+if myMon == "February":
+  print("February is kinda short right", name)
+if myMon == "March":
+  print("OOO thats Kunjs Birthday", name)
+if myMon == "April":
+  print(name,"did you get fooled on April Fools Day!")
+if myMon == "May":
+  print(name, "if summer hasnt started yet then im going to crash out")
+    
+
+elif name == "David" or name== "david":
+ DOW = input("What is the day of the week? ")
+ if DOW == "monday" or DOW == "Monday":
+   print("It is going to be a great Monday", name)
+ if DOW == "tuesday" or DOW == "Tuesday":
+   print("You look great in that color", name)
+ if DOW == "wednesday" or DOW == "Wednesday":
+   print("You look chipper today", name)
+ if DOW == "thursday" or DOW == "Thursday":
+   print(name,"you are doing a great job!")
+ if DOW == "friday" or DOW == "Friday":
+   print(name, "it's FRIDAY!")
+else:
+ print("I do not know your name, but I hope you are having a great day!")
